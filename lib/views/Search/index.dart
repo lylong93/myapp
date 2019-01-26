34475a5d7d84d92d5d54bcac0f1d34978612d505
lyl <695498118@qq.com>
2019-01-26 23:57:20 +0800

    searchshow

--- lib/views/Search/index.dart
+++ lib/views/Search/index.dart
@@ -1,14 +1,24 @@
 import 'package:flutter/material.dart';
 class Search extends SearchDelegate {
 
-  Widget  buildActions()
-
-
-  Widget  buildLeading() 
-
-  // }
-  Widget buildResults() 
-
-  Widget buildSuggestions() 
+ @override
+ List<Widget> buildActions(BuildContext context) {
+    return [
+      Text('o')
+    ];
   }
+  @override
+  Widget  buildLeading(BuildContext context) {
+    return Icon(Icons.golf_course);
+  }
+
+ @override
+  Widget buildResults(BuildContext context){
+    return Text('ok');
+  }
+ @override
+  Widget buildSuggestions(BuildContext context){
+    return Text('buildSuggestions');
+  }
+
 }
