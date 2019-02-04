2966d3e56a35607fb4d24e5ca5f7294f1c06df18
lyl <695498118@qq.com>
2019-02-04 18:23:29 +0800

    img

--- lib/models/home.dart
+++ lib/models/home.dart
@@ -1,16 +1,18 @@
 class HomeItem {
   final String name;
   final String text;
-
-  HomeItem(this.name, this.text);
+  final String bgimg; 
+  HomeItem(this.name, this.text,this.bgimg);
 
   HomeItem.fromJson(Map<String, dynamic> json)
       : name = json['name'],
+        bgimg= json['bgimg'],
         text = json['text'];
 
   Map<String, dynamic> toJson() =>
     {
       'name': name,
       'email': text,
+      'bgimg':bgimg
     };
 }
