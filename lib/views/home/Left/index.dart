1dcd085c9bf5418cadeb4d01c35033268189bd8b
lyl <695498118@qq.com>
2019-02-07 23:26:35 +0800

    list build

--- lib/views/home/Left/index.dart
+++ lib/views/home/Left/index.dart
@@ -13,15 +13,27 @@ class HomeLeft extends StatefulWidget {
 }
 
 class _HomeLeftState extends State<HomeLeft> {
-  List<Widget> childList = [];
+  List<Widget> childList = [LeftCard(name: 'name')];
+  String _size = '0';
   ScrollController _scrollController = new ScrollController();
 
   @override
   void initState() {
-    getdata();
     super.initState();
+    // getdata();
     _scrollController.addListener(() {
-       getdata();
+       if (_scrollController.position.pixels ==
+          _scrollController.position.maxScrollExtent) {
+         childList.add(LeftCard(name: 'name'));
+          print('滑动到了最底部');
+
+        setState(() {        
+        // getdata();
+        });
+        //  getdata();
+        // childList.clear();
+      }
+      
     });
   }
 
@@ -29,33 +41,40 @@ class _HomeLeftState extends State<HomeLeft> {
     var jsonString = await HomeApi.getTest();
     var data = jsonString['list'];
     List<Widget> _end = [];
-    // childList.clear();
     data.forEach((item) {
       var user = new HomeItem.fromJson(item);
       _end..add(LeftCard(name: 'name'));
     });
-
     setState(() {
-      childList = _end;
+      _size+= '0.1';
+      // childList = childList.insert(1,Text('data'));
+      childList.add(LeftCard(name: 'name'));
+      print(childList);
     });
   }
 
   Future _loadRefresh() async {
     await Future.delayed(Duration(seconds: 2), () {
-      setState(() {
-        childList.clear();
+      setState(() {        
         getdata();
-        return null;
       });
     });
   }
-
+_build () {
+  return [this.childList.length];
+}
   @override
   Widget build(BuildContext context) {
+    print(childList);
     return RefreshIndicator(
       color: Color.fromRGBO(1, 1, 1, .1),
-      child: ListView(
-        children: childList,
+      child: ListView.builder(
+        // itemCount: widgets.length,
+        itemCount: childList.length,
+        physics: const AlwaysScrollableScrollPhysics(),
+        itemBuilder: (context, index){
+          return LeftCard(name:'po');
+        },
         controller:_scrollController
       ),
       onRefresh: _loadRefresh,
