971b4e0bd6e67a54e531f02c72224dfe0b06b28b
lyl <695498118@qq.com>
2019-02-13 23:29:28 +0800

    test

--- lib/views/Shop/index.dart
+++ lib/views/Shop/index.dart
@@ -15,103 +15,121 @@ class _ShopScreenState extends State<ShopScreen>
   Animation animation;
 
   ScrollController _controller = ScrollController();
+  ScrollController _nestcontroller = ScrollController();
   @override
   void initState() {
     super.initState();
     _controller.addListener(() {
       print('ooo');
     });
+    _nestcontroller.addListener(() {
+      // print(_nestcontroller.position.pixels);
+      // controller.forward();
+    });
     controller = AnimationController(
       vsync: this,
       duration: const Duration(seconds: 3),
     );
-    animation = Tween(begin: 0.0, end: 100.0).animate(controller)
+    animation = Tween(begin: 100.0, end:200.0).animate(controller)
       ..addListener(() {
         // print('object');
       });
     // controller.forward();
     // curved = new CurvedAnimation(
-    //     parent: controller, curve: Curves.linear); //模仿小球自由落体运动轨迹
+    // parent: controller, curve: Curves.linear); //模仿小球自由落体运动轨迹
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: NestedScrollView(
-      headerSliverBuilder: (BuildContext context, bool innerBoxIsScrolled) {
-        return [
-          SliverOverlapAbsorber(
-            handle: NestedScrollView.sliverOverlapAbsorberHandleFor(context),
-            child: SliverAppBar(
-                pinned: true,
-
-                expandedHeight: 300.0,
-                flexibleSpace: Container(
-                    color: Colors.blue,
-                    height: 300.0,
-                    child: Column(
-                      children: <Widget>[
-                        AppBar(
-                          title: Text("this is ok"),
-                          elevation:0.0
-                        ),
-                        Expanded(
-                          child: ListView(
-                            physics: const AlwaysScrollableScrollPhysics(),
-                            scrollDirection: Axis.horizontal,
-                            children: <Widget>[
-                              Container(
-                                // color: Colors.yellow,
-                                width: 80.0,
-                                child: Image.network(
-                                    'http://pic.zhutou.com/html/UploadPic/2010-6/2010664120959.jpg'),
-                              ),
-                              Container(
-                                margin: EdgeInsets.all(10.0),
-                                width: 80.0,
-                                child: Image.network(
-                                    'http://pic.zhutou.com/html/UploadPic/2010-6/2010664120959.jpg'),
-                              ),
-                              Container(
-                                // color: Colors.yellow,
-                                width: 130.0,
-                                child: Image.network(
-                                    'http://pic.zhutou.com/html/UploadPic/2010-6/2010664120959.jpg'),
-                              ),
-                               Container(
-                                // color: Colors.yellow,
-                                 margin: EdgeInsets.all(10.0),
-                                width: 80.0,
-                                child: Image.network(
-                                    'http://pic.zhutou.com/html/UploadPic/2010-6/2010664120959.jpg'),
-                              ),
-                               Container(
-                                // color: Colors.yellow,
-                                width: 80.0,
-                                child: Image.network(
-                                    'http://pic.zhutou.com/html/UploadPic/2010-6/2010664120959.jpg'),
-                              ),
-                            ],
-                            controller: _controller,
-                          ),
-                        )
-                      ],
-                    ))),
-          )
-        ];
-      },
-      body: 
-
-
-      AnimatedBuilder(
+      body: NestedScrollView(
+        controller: _nestcontroller,
+        headerSliverBuilder: (BuildContext context, bool innerBoxIsScrolled) {
+          return [
+            SliverOverlapAbsorber(
+              handle: NestedScrollView.sliverOverlapAbsorberHandleFor(context),
+              child: SliverAppBar(
+                  pinned: true,
+                  expandedHeight: 300.0,
+                  flexibleSpace: Container(
+                      color: Colors.blue,
+                      height: 300.0,
+                      child: Column(
+                        children: <Widget>[
+                          AppBar(title: Text("this is ok"), elevation: 0.0),
+                          Expanded(
+                            child: ListView(
+                              physics: const AlwaysScrollableScrollPhysics(),
+                              scrollDirection: Axis.horizontal,
+                              children: <Widget>[
+                                Container(
+                                  // color: Colors.yellow,
+                                  width: 80.0,
+                                  child: Image.network(
+                                      'http://pic.zhutou.com/html/UploadPic/2010-6/2010664120959.jpg'),
+                                ),
+                                Container(
+                                  margin: EdgeInsets.all(10.0),
+                                  width: 80.0,
+                                  child: Image.network(
+                                      'http://pic.zhutou.com/html/UploadPic/2010-6/2010664120959.jpg'),
+                                ),
+                                Container(
+                                  // color: Colors.yellow,
+                                  width: 130.0,
+                                  child: Image.network(
+                                      'http://pic.zhutou.com/html/UploadPic/2010-6/2010664120959.jpg'),
+                                ),
+                                Container(
+                                  // color: Colors.yellow,
+                                  margin: EdgeInsets.all(10.0),
+                                  width: 80.0,
+                                  child: Image.network(
+                                      'http://pic.zhutou.com/html/UploadPic/2010-6/2010664120959.jpg'),
+                                ),
+                                Container(
+                                  // color: Colors.yellow,
+                                  width: 80.0,
+                                  child: Image.network(
+                                      'http://pic.zhutou.com/html/UploadPic/2010-6/2010664120959.jpg'),
+                                ),
+                              ],
+                              controller: _controller,
+                            ),
+                          )
+                        ],
+                      ))),
+            )
+          ];
+        },
+        body:
+        AnimatedBuilder(
           animation: animation,
           builder: (context, child) => Container(
-                margin: EdgeInsets.only(top:100.0),
-                child: Text('abc'),
+                margin: EdgeInsets.only(top: 100.0),
+                // color: animation.value,
+                decoration: BoxDecoration(
+                  border: Border(
+                    top: BorderSide(color: Colors.red,),
+                    bottom: BorderSide(color: Colors.red,)
+                  )
+                ),
+                child:
+                
+                 ListView(
+                  children: <Widget>[
+                    Text('p')
+                  ],
+                ),
               ),
         ),
-    )
-
-        );
+        // Container(
+        //   color: Colors.red,
+        //   margin: EdgeInsets.only(top: 100.0),
+        //   child: Text('o'),
+        // ),
+        
+      ),
+    );
   }
 }
