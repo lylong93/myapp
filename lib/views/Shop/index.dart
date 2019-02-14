fe3c8940837b80797070ce50af374038cea8e4a3
lyl <695498118@qq.com>
2019-02-14 23:22:35 +0800

    test

--- lib/views/Shop/index.dart
+++ lib/views/Shop/index.dart
@@ -1,6 +1,72 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/animation.dart';
 
+// void change (n){
+//   setState(() {});
+// }
+// class World extends StatefulWidget {
+//   @override
+//   State<StatefulWidget> createState() => _CircleProgressState();
+// }
+// class _CircleProgressState extends State<World> {
+//   int flag = 1;
+//   @override
+//   void initState() {
+//   void change (n){
+//     // setState(() {});
+//     print('object');
+//   }
+//   }
+  
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return Text(flag.toString());
+//   }
+// }
+
+
+// 画布
+class Sky extends CustomPainter {
+  Sky({this.of});
+  var of;
+  @override
+  void paint(Canvas canvas, Size size) {
+    var rect = Offset.zero & size;
+    var gradient = RadialGradient(
+      center: const Alignment(0.7, -0.6),
+      radius: 0.2,
+      colors: [const Color(0xFFFFFF00), const Color(0xFF0099FF)],
+      stops: [0.4, 1.0],
+    );
+  Paint paint = new Paint();
+  paint.strokeWidth = 10;
+  paint.style = PaintingStyle.stroke;
+    // canvas.drawRect(
+    //   rect,
+    //   Paint()..shader = gradient.createShader(rect),
+    // );
+    canvas.drawLine(
+      Offset(0.0,of),
+      Offset(375.0,of),
+      paint
+      // Paint()..shader = gradient.createShader(rect),
+    );
+  }
+
+  @override
+  SemanticsBuilderCallback get semanticsBuilder {
+    return (Size size) {};
+  }
+
+  @override
+  bool shouldRepaint(Sky oldDelegate) => true;
+  @override
+  bool shouldRebuildSemantics(Sky oldDelegate) => false;
+}
+
+
+// main
 class ShopScreen extends StatefulWidget {
   const ShopScreen({Key key}) : super(key: key);
 
@@ -11,29 +77,111 @@ class ShopScreen extends StatefulWidget {
 class _ShopScreenState extends State<ShopScreen>
     with SingleTickerProviderStateMixin {
   AnimationController controller;
-  CurvedAnimation curved; //曲线动画，动画插值，
+  // CurvedAnimation curved; //曲线动画，动画插值，
   Animation animation;
 
   ScrollController _controller = ScrollController();
   ScrollController _nestcontroller = ScrollController();
+// AnimationController _nestcontroller = AnimationController();
+  int n = 1;
+  double of = 0.0;
+
+  var _start = 0.0;
+  var _next = 0.0;
+
+  var istop = true;
+  // var isbom = false;
+
   @override
   void initState() {
     super.initState();
-    _controller.addListener(() {
-      print('ooo');
-    });
+    // _controller.addListener(() {
+    //   print('ooo');
+    // });
+    // _controler = AnimationController(
+    //   vsync: this,
+    //   duration: Duration(milliseconds: 300),
+    //   // the SingleTickerProviderStateMixin
+    //   // duration: widget.duration,
+    // );
+    // _controler.addListener(() {
+    //   print('object');
+    // });
+    // AnimationController();
     _nestcontroller.addListener(() {
+      // controller.animateTo();
+     
+      var pix  = _nestcontroller.position.pixels;
+      if(pix == 244)  {
+        print('到顶部了');
+        controller.reset();
+           controller.forward();
+          istop = true; 
+          
+          //  controller.reset();
+          //  controller.forward();   
+           
+            // print(istop);
+          // _start = 0;
+          // _next = 0;
+      } 
+      if(pix == 0)  {
+        
+         print('到底部了');
+         controller.reset();
+        controller.forward();
+         istop = false;
+         
+          // controller.reset();
+        //  controller.forward();
+          
+          // _start = 0;
+          // _next = 0;
+      } 
+      // if(istop) {
+        // if(_start == 0 ) {
+        //   _start = pix;
+        //   }
+        // _next = pix;
+      // var off = _start - _next;
+      // if(off< 0) {
+      //    print('上');
+      //   // of= of+5.0;
+      //   // print(of);
+      //   // print(controller);
+      //   // controller.forward();
+      //   // controller;
+      // } else if(off > 0){
+      //   print('下');
+      //   // of= of-5.0;
+      //   // print(of);
+      // }
+      // setState(() {});
       // print(_nestcontroller.position.pixels);
       // controller.forward();
     });
     controller = AnimationController(
       vsync: this,
       duration: const Duration(seconds: 3),
-    );
-    animation = Tween(begin: 100.0, end:200.0).animate(controller)
-      ..addListener(() {
-        // print('object');
-      });
+    )
+    ..addListener(() {
+      // print('object');
+      // print('动画');
+      if(!istop) {
+        print('减少');
+        // of++;
+        of--;
+      } else {
+        print('增加');
+        //  of--;
+        of++;
+      }
+      setState((){});
+    });
+    // animation = Tween(begin: 100.0, end: 200.0).animate(controller)
+    //   ..addListener(() {
+    //     // print('object');
+    //   });
     // controller.forward();
     // curved = new CurvedAnimation(
     // parent: controller, curve: Curves.linear); //模仿小球自由落体运动轨迹
@@ -41,6 +189,7 @@ class _ShopScreenState extends State<ShopScreen>
 
   @override
   Widget build(BuildContext context) {
+
     return Scaffold(
       body: NestedScrollView(
         controller: _nestcontroller,
@@ -102,34 +251,64 @@ class _ShopScreenState extends State<ShopScreen>
             )
           ];
         },
-        body:
-        AnimatedBuilder(
-          animation: animation,
-          builder: (context, child) => Container(
-                margin: EdgeInsets.only(top: 100.0),
-                // color: animation.value,
-                decoration: BoxDecoration(
-                  border: Border(
-                    top: BorderSide(color: Colors.red,),
-                    bottom: BorderSide(color: Colors.red,)
-                  )
+        body: 
+        Container(
+          margin: EdgeInsets.only(top: 100.0),
+           child:CustomPaint(
+                painter: Sky(
+                  of:of
                 ),
-                child:
-                
-                 ListView(
-                  children: <Widget>[
-                    Text('p')
-                  ],
-                ),
-              ),
-        ),
+              )
+        )
+
+        
+        // AnimatedBuilder(
+        //   animation: animation,
+        //   builder: (context, child) => Container(
+        //       margin: EdgeInsets.only(top: 100.0),
+        //       // color: animation.value,
+        //       decoration: BoxDecoration(
+        //           border: Border(
+        //               top: BorderSide(
+        //                 color: Colors.red,
+        //               ),
+        //               bottom: BorderSide(
+        //                 color: Colors.red,
+        //               ))),
+        //       child: 
+        //       // Text(n.toString())
+        //       CustomPaint(
+        //         painter: Sky(
+        //           of:of
+        //         ),
+        //       )
+
+        //       ),
+        // ),
         // Container(
         //   color: Colors.red,
         //   margin: EdgeInsets.only(top: 100.0),
         //   child: Text('o'),
         // ),
-        
       ),
+      // floatingActionButton:FloatingActionButton(
+      //   onPressed: () {
+      //     if(istop) {
+      //       controller.reset();
+      //       print('istop');
+      //       istop = false;
+      //       controller.forward();
+
+      //     } else {
+      //        controller.reset();
+      //        print('向后播放动画');
+      //       controller.forward();//向后播放动画
+           
+      //     }
+             
+      //   },
+      //   child: new Icon(Icons.track_changes),
+      // )
     );
   }
 }
