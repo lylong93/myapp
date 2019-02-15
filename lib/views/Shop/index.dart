b64c53c37620dd01bf29c7076264c0ba144a8c8e
lyl <695498118@qq.com>
2019-02-15 23:38:16 +0800

    test

--- lib/views/Shop/index.dart
+++ lib/views/Shop/index.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/animation.dart';
+import 'dart:math' as Math;
 
 // void change (n){
 //   setState(() {});
@@ -17,7 +18,6 @@ import 'package:flutter/animation.dart';
 //     print('object');
 //   }
 //   }
-  
 
 //   @override
 //   Widget build(BuildContext context) {
@@ -25,33 +25,94 @@ import 'package:flutter/animation.dart';
 //   }
 // }
 
-
 // 画布
 class Sky extends CustomPainter {
-  Sky({this.of});
+  Sky({this.of,this.dx,this.rd});
   var of;
+  var dx;
+  var rd;
   @override
   void paint(Canvas canvas, Size size) {
-    var rect = Offset.zero & size;
-    var gradient = RadialGradient(
-      center: const Alignment(0.7, -0.6),
-      radius: 0.2,
-      colors: [const Color(0xFFFFFF00), const Color(0xFF0099FF)],
-      stops: [0.4, 1.0],
-    );
-  Paint paint = new Paint();
-  paint.strokeWidth = 10;
-  paint.style = PaintingStyle.stroke;
-    // canvas.drawRect(
-    //   rect,
-    //   Paint()..shader = gradient.createShader(rect),
+    // var rect = Offset.zero & size;
+    // var rect = Rect.fromCircle(center: Offset(50.0,50.0),radius: 10.0);
+    var rect = Rect.fromLTRB(0.0, 0.0, 375.0, 300.0);
+
+    var gradient = LinearGradient(
+      begin: Alignment.topLeft,
+      end:Alignment.bottomLeft,
+       colors: [const Color(0xFFFFFF00), const Color(0xFF0099FF)],
+       stops: [0.4, 1.0],
+      );
+    
+    // RadialGradient(
+    //   center: const Alignment(0.0, 0.0),
+    //   radius: rd,
+    //   colors: [const Color(0xFFFFFF00), const Color(0xFF0099FF)],
+    //   stops: [0.4, 1.0],
     // );
-    canvas.drawLine(
-      Offset(0.0,of),
-      Offset(375.0,of),
-      paint
-      // Paint()..shader = gradient.createShader(rect),
+    Paint paint = new Paint()
+      ..strokeWidth = 10
+      ..style = PaintingStyle.fill
+      ..shader = gradient.createShader(rect);
+
+    Paint linepaint = new Paint();
+    linepaint.strokeWidth = 1;
+    linepaint.style = PaintingStyle.stroke;
+    linepaint.color = Colors.red;
+    // linepaint.shader = gradient.createShader(rect);
+
+    // canvas.drawLine(
+    //   Offset(0.0, 100.0),
+
+    //   Offset(375.0, 100.0),
+    //   linepaint);
+    var path = Path();
+    path.reset();
+     path.moveTo(0.0, 600.0);
+    path.lineTo(0.0, 100.0);
+    path.cubicTo(0.0, 0.0, dx, of, 375.0, 100.0);
+     path.lineTo(375.0, 600.0);
+    path.close();
+    // Path path = new Path();
+
+    //  var width = 200;
+    //  var height = 300;
+    //  path.moveTo(0.0, 0.0);
+    //  path.lineTo(width.toDouble(), height.toDouble());
+    //  path.cubicTo((width * 6) / 7, height / 9, (width * 12) / 13,
+    //      (height * 2) / 5, width / 2, (height * 7) / 12);
+    //  canvas.drawPath(path, linepaint);
+
+
+  
+
+    canvas.clipPath(
+      path,
+      doAntiAlias:true,
     );
+      
+    canvas.drawRect(rect,paint);
+    
+    // canvas.drawRect(rect1,linepaint);
+
+
+    // canvas.drawPath(path, linepaint);
+
+    // canvas.clipRect(
+    //   rect,
+    //   doAntiAlias:true,
+    // );
+    // canvas.clipPath(
+    //   path,
+    //   doAntiAlias:true,
+    //   );
+     
+    // canvas.drawLine(
+    //   Offset(0.0,of),
+    //   Offset(375.0,of),
+    //   paint
+    //   // Paint()..shader = gradient.createShader(rect),
+    // );
   }
 
   @override
@@ -64,7 +125,6 @@ class Sky extends CustomPainter {
   @override
   bool shouldRebuildSemantics(Sky oldDelegate) => false;
 }
-
 
 // main
 class ShopScreen extends StatefulWidget {
@@ -84,11 +144,13 @@ class _ShopScreenState extends State<ShopScreen>
   ScrollController _nestcontroller = ScrollController();
 // AnimationController _nestcontroller = AnimationController();
   int n = 1;
-  double of = 0.0;
-
+  double of = Math.Random().nextDouble();
+  //  of= Math.Random;
+   double dx = 0.0;
+  double rd = 0.1;
   var _start = 0.0;
   var _next = 0.0;
-
+  var  user ;
   var istop = true;
   // var isbom = false;
 
@@ -109,40 +171,41 @@ class _ShopScreenState extends State<ShopScreen>
     // });
     // AnimationController();
     _nestcontroller.addListener(() {
-      // controller.animateTo();
      
-      var pix  = _nestcontroller.position.pixels;
-      if(pix == 244)  {
+      print( _nestcontroller.position);
+      // controller.animateTo();
+
+      var pix = _nestcontroller.position.pixels;
+      if (pix == 244) {
         print('到顶部了');
         controller.reset();
-           controller.forward();
-          istop = true; 
-          
-          //  controller.reset();
-          //  controller.forward();   
-           
-            // print(istop);
-          // _start = 0;
-          // _next = 0;
-      } 
-      if(pix == 0)  {
-        
-         print('到底部了');
-         controller.reset();
         controller.forward();
-         istop = false;
-         
-          // controller.reset();
+        istop = true;
+
+        //  controller.reset();
         //  controller.forward();
-          
-          // _start = 0;
-          // _next = 0;
-      } 
+
+        // print(istop);
+        // _start = 0;
+        // _next = 0;
+      }
+      if (pix == 0) {
+        print('到底部了');
+        controller.reset();
+        controller.forward();
+        istop = false;
+
+        // controller.reset();
+        //  controller.forward();
+
+        // _start = 0;
+        // _next = 0;
+      }
       // if(istop) {
-        // if(_start == 0 ) {
-        //   _start = pix;
-        //   }
-        // _next = pix;
+      // if(_start == 0 ) {
+      //   _start = pix;
+      //   }
+      // _next = pix;
       // var off = _start - _next;
       // if(off< 0) {
       //    print('上');
@@ -163,21 +226,26 @@ class _ShopScreenState extends State<ShopScreen>
     controller = AnimationController(
       vsync: this,
       duration: const Duration(seconds: 3),
-    )
-    ..addListener(() {
-      // print('object');
-      // print('动画');
-      if(!istop) {
-        print('减少');
-        // of++;
-        of--;
-      } else {
-        print('增加');
-        //  of--;
-        of++;
-      }
-      setState((){});
-    });
+    )..addListener(() {
+        // print('object');
+        // print('动画');
+        if (!istop) {
+          // print('减少');
+          of++;
+          rd = rd +0.01;
+          // of--;
+          // of = of --;
+          // of= of-0.1;
+        } else {
+          // print('增加');
+          of--;
+           rd = rd -0.01;
+          // of++;
+          // of = of ++;
+          //  of= of+0.1;
+        }
+        setState(() {});
+      });
     // animation = Tween(begin: 100.0, end: 200.0).animate(controller)
     //   ..addListener(() {
     //     // print('object');
@@ -189,126 +257,151 @@ class _ShopScreenState extends State<ShopScreen>
 
   @override
   Widget build(BuildContext context) {
-
     return Scaffold(
-      body: NestedScrollView(
-        controller: _nestcontroller,
-        headerSliverBuilder: (BuildContext context, bool innerBoxIsScrolled) {
-          return [
-            SliverOverlapAbsorber(
-              handle: NestedScrollView.sliverOverlapAbsorberHandleFor(context),
-              child: SliverAppBar(
-                  pinned: true,
-                  expandedHeight: 300.0,
-                  flexibleSpace: Container(
-                      color: Colors.blue,
-                      height: 300.0,
-                      child: Column(
-                        children: <Widget>[
-                          AppBar(title: Text("this is ok"), elevation: 0.0),
-                          Expanded(
-                            child: ListView(
-                              physics: const AlwaysScrollableScrollPhysics(),
-                              scrollDirection: Axis.horizontal,
-                              children: <Widget>[
-                                Container(
-                                  // color: Colors.yellow,
-                                  width: 80.0,
-                                  child: Image.network(
-                                      'http://pic.zhutou.com/html/UploadPic/2010-6/2010664120959.jpg'),
-                                ),
-                                Container(
-                                  margin: EdgeInsets.all(10.0),
-                                  width: 80.0,
-                                  child: Image.network(
-                                      'http://pic.zhutou.com/html/UploadPic/2010-6/2010664120959.jpg'),
-                                ),
-                                Container(
-                                  // color: Colors.yellow,
-                                  width: 130.0,
-                                  child: Image.network(
-                                      'http://pic.zhutou.com/html/UploadPic/2010-6/2010664120959.jpg'),
-                                ),
-                                Container(
-                                  // color: Colors.yellow,
-                                  margin: EdgeInsets.all(10.0),
-                                  width: 80.0,
-                                  child: Image.network(
-                                      'http://pic.zhutou.com/html/UploadPic/2010-6/2010664120959.jpg'),
-                                ),
-                                Container(
-                                  // color: Colors.yellow,
-                                  width: 80.0,
-                                  child: Image.network(
-                                      'http://pic.zhutou.com/html/UploadPic/2010-6/2010664120959.jpg'),
-                                ),
-                              ],
-                              controller: _controller,
-                            ),
-                          )
-                        ],
-                      ))),
-            )
-          ];
-        },
-        body: 
-        Container(
-          margin: EdgeInsets.only(top: 100.0),
-           child:CustomPaint(
-                painter: Sky(
-                  of:of
-                ),
-              )
-        )
-
+        body: Listener(
+      // onScaleStart: test,
+      onPointerDown: (event) {
         
-        // AnimatedBuilder(
-        //   animation: animation,
-        //   builder: (context, child) => Container(
-        //       margin: EdgeInsets.only(top: 100.0),
-        //       // color: animation.value,
-        //       decoration: BoxDecoration(
-        //           border: Border(
-        //               top: BorderSide(
-        //                 color: Colors.red,
-        //               ),
-        //               bottom: BorderSide(
-        //                 color: Colors.red,
-        //               ))),
-        //       child: 
-        //       // Text(n.toString())
-        //       CustomPaint(
-        //         painter: Sky(
-        //           of:of
-        //         ),
-        //       )
+        user = event.position;
+        print(user.dx);
+        dx = user.dx;
+      },
+      child:
+          // Container(
+          //   color: Colors.red,
+          //   height: 100.0,
+          //   width: 100.0,
+          //   child: Text('data'),
+          // )
+          NestedScrollView(
+              controller: _nestcontroller,
+              headerSliverBuilder:
+                  (BuildContext context, bool innerBoxIsScrolled) {
+                return [
+                  SliverOverlapAbsorber(
+                    handle: NestedScrollView.sliverOverlapAbsorberHandleFor(
+                        context),
+                    child: SliverAppBar(
+                        pinned: true,
+                        expandedHeight: 300.0,
+                        flexibleSpace: Container(
+                            color: Colors.blue,
+                            height: 300.0,
+                            child: Column(
+                              children: <Widget>[
+                                AppBar(
+                                    title: Text("this is ok"), elevation: 0.0),
+                                Expanded(
+                                  child: ListView(
+                                    physics:
+                                        const AlwaysScrollableScrollPhysics(),
+                                    scrollDirection: Axis.horizontal,
+                                    children: <Widget>[
+                                      Container(
+                                        // color: Colors.yellow,
+                                        width: 80.0,
+                                        child: Image.network(
+                                            'http://pic.zhutou.com/html/UploadPic/2010-6/2010664120959.jpg'),
+                                      ),
+                                      Container(
+                                        margin: EdgeInsets.all(10.0),
+                                        width: 80.0,
+                                        child: Image.network(
+                                            'http://pic.zhutou.com/html/UploadPic/2010-6/2010664120959.jpg'),
+                                      ),
+                                      Container(
+                                        // color: Colors.yellow,
+                                        width: 130.0,
+                                        child: Image.network(
+                                            'http://pic.zhutou.com/html/UploadPic/2010-6/2010664120959.jpg'),
+                                      ),
+                                      Container(
+                                        // color: Colors.yellow,
+                                        margin: EdgeInsets.all(10.0),
+                                        width: 80.0,
+                                        child: Image.network(
+                                            'http://pic.zhutou.com/html/UploadPic/2010-6/2010664120959.jpg'),
+                                      ),
+                                      Container(
+                                        // color: Colors.yellow,
+                                        width: 80.0,
+                                        child: Image.network(
+                                            'http://pic.zhutou.com/html/UploadPic/2010-6/2010664120959.jpg'),
+                                      ),
+                                    ],
+                                    controller: _controller,
+                                  ),
+                                )
+                              ],
+                            ))),
+                  )
+                ];
+              },
+              body: 
+              
+              Container(
+                  margin: EdgeInsets.only(top: 100.0),
+                  child: 
+                  
+                  CustomPaint(
+                    painter: Sky(of:of,dx:dx,rd:rd),
+                    child: ListView(
+                      children: <Widget>[
+                        Text('o'),
+                        Text('k'),
+                      ],
+                    ),
+                  )
+                  )
+                  ),
+      // AnimatedBuilder(
+      //   animation: animation,
+      //   builder: (context, child) => Container(
+      //       margin: EdgeInsets.only(top: 100.0),
+      //       // color: animation.value,
+      //       decoration: BoxDecoration(
+      //           border: Border(
+      //               top: BorderSide(
+      //                 color: Colors.red,
+      //               ),
+      //               bottom: BorderSide(
+      //                 color: Colors.red,
+      //               ))),
+      //       child:
+      //       // Text(n.toString())
+      //       CustomPaint(
+      //         painter: Sky(
+      //           of:of
+      //         ),
+      //       )
 
-        //       ),
-        // ),
-        // Container(
-        //   color: Colors.red,
-        //   margin: EdgeInsets.only(top: 100.0),
-        //   child: Text('o'),
-        // ),
-      ),
-      // floatingActionButton:FloatingActionButton(
-      //   onPressed: () {
-      //     if(istop) {
-      //       controller.reset();
-      //       print('istop');
-      //       istop = false;
-      //       controller.forward();
+      //       ),
+      // ),
+      // Container(
+      //   color: Colors.red,
+      //   margin: EdgeInsets.only(top: 100.0),
+      //   child: Text('o'),
+      // ),
+    )
 
-      //     } else {
-      //        controller.reset();
-      //        print('向后播放动画');
-      //       controller.forward();//向后播放动画
-           
-      //     }
-             
-      //   },
-      //   child: new Icon(Icons.track_changes),
-      // )
-    );
+        // floatingActionButton:FloatingActionButton(
+        //   onPressed: () {
+        //     if(istop) {
+        //       controller.reset();
+        //       print('istop');
+        //       istop = false;
+        //       controller.forward();
+
+        //     } else {
+        //        controller.reset();
+        //        print('向后播放动画');
+        //       controller.forward();//向后播放动画
+
+        //     }
+
+        //   },
+        //   child: new Icon(Icons.track_changes),
+        // )
+        );
   }
 }
